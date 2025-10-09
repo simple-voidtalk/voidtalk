04a1a5858fd7cdc4fed0b91d7058ac17a650b2cd
Fire <badhead55@web.de>
2025-10-09 18:17:12 +0200

    added basic home nav menu

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -1,9 +1,68 @@
-import { HomeNavigationMenu } from "@/components/home-navigation-menu";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@voidtalk/ui/components/sidebar";
+import { Home, Search, Settings } from "lucide-react";
 
 export default function Page() {
+    const items = [
+        {
+            title: "Home",
+            url: "#",
+            icon: Home,
+        },
+        {
+            title: "Search",
+            url: "#",
+            icon: Search,
+        },
+        {
+            title: "Settings",
+            url: "#",
+            icon: Settings,
+        },
+    ];
+
     return (
-        <div>
-            <HomeNavigationMenu />
-        </div>
+        <SidebarProvider>
+            <Sidebar variant={"inset"}>
+                <SidebarHeader>"Logo"</SidebarHeader>
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupLabel>Navigation</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <a href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>
+                <SidebarFooter>"Profile"</SidebarFooter>
+            </Sidebar>
+            <div className={"flex flex-col w-full"}>
+                <header className={"flex justify-end items-center"}>
+                    <SidebarTrigger variant={"secondary"}/>
+                </header>
+            </div>
+        </SidebarProvider>
     );
 }
